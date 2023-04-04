a04953deb8256deee46900d910e0240675d40590
MoGeSMa1 <arseniyzaikin123456789@gmail.com>
2023-04-04 18:28:12 +0300

    2.0

--- Python_02/main.py
+++ Python_02/main.py
@@ -1,11 +1,7 @@
-height = int(input("Введите ваш рост - "))
+height = int(input('Введите ваш рост - '))
 weight = int(input('Введите ваш вес - '))
 BMI = round((weight/(height*height))*10000, 2)
 print('Ваш индекс массы тела - ', BMI)
 BMI= round(BMI)
-skale = ['20 ', "= "*30, ' 50']
-skale = ''.join(skale)
-skale = skale.split(' ')
-skale[BMI-20] = '|'
-skale = ''.join(skale)
-print(skale)
+skale = '20' + '='*28 + '50'
+print(skale[:BMI-20], '|', skale[BMI-20:], sep='=')
